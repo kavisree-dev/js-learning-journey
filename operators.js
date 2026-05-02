d51c7c618be79298b6ff2535dc8954328441df5a
kavisree-dev <kaviamuthan1405@gmail.com>
2026-05-02 23:17:55 +0530

    operators practice ==Arithmetic--increment and decrement

---
+++ operators.js
@@ -0,0 +1,21 @@
+/*increment oprator(++) 
+  decrement operator(--)  --> Arithmetic operator
+used in two ways*/
+var a=5;
+a=a++;//post increment
+c=a++;
+d=++a;//pre increment
+
+console.log(a);
+console.log(c);
+console.log(d);
+
+//pre decrement
+var b=5;
+b=b--;// post increment
+f=b--;
+s=--b;// pre increment
+console.log(b);
+console.log(f);
+console.log(s);
+
